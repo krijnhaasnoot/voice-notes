545a65dbe0e44e606f9a0bc568b7a601be4c7673
Krijn Haasnoot <krijnhaasnoot@gmail.com>
2025-09-06 16:54:51 +0200

    Initial Commit

---
+++ Voice Notes/Voice_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_NotesApp.swift
+//  Voice Notes
+//
+//  Created by Krijn Haasnoot on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Voice_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Voice NotesTests/Voice_NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_NotesTests.swift
+//  Voice NotesTests
+//
+//  Created by Krijn Haasnoot on 06/09/2025.
+//
+
+import Testing
+@testable import Voice_Notes
+
+struct Voice_NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
